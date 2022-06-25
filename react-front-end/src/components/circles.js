f43033e413d567d32483e1ffb48608257a4f09d6
AliOlliek <olliek95@gmail.com>
2022-06-25 21:18:38 +0300

    Styled and Animated Signup/Login

---
+++ react-front-end/src/components/circles.js
@@ -0,0 +1,17 @@
+// Login/Signup Animation Component
+
+import React from 'react'
+
+export default function Circles() {
+  return (
+    <div className='area'>
+    <ul className='floating'>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    </div>
+  )
+}
