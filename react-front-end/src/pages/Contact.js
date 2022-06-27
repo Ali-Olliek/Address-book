63ef73026d906fc8f570d3b86112b5117730e9a2
AliOlliek <olliek95@gmail.com>
2022-06-27 10:32:32 +0300

    Removed Status from Contact Page

--- react-front-end/src/pages/Contact.js
+++ react-front-end/src/pages/Contact.js
@@ -9,7 +9,6 @@ export default function Contact({contact}) {
         <h1>Contact Name: {contact.name}</h1>
         <p>Contact Email: {contact.email}</p>
         <p>Contact Phone Number: {contact.phone_number}</p>
-        <p>Marital Status: {(contact.marital_status) && (<p>Married</p>) ? Married : "Married"}</p>
         <div>
           <Map name ={contact.name} location={contact.contact_location}/>
         </div>
